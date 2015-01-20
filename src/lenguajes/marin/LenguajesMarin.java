045e409a7db1ca4c6a9dafddcb9ec5f224e60ca8
T-107 <T-107@PC249>
2015-01-19 19:28:58 -0600

    mi primer push a repositorio

---
+++ src/lenguajes/marin/LenguajesMarin.java
@@ -0,0 +1,14 @@
+
+package lenguajes.marin;
+
+
+public class LenguajesMarin {
+
+ 
+    public static void main(String[] args) {
+      
+        System.out.println("MI PRIMER PROGRA DE JAVA II");
+        
+    }
+    
+}
