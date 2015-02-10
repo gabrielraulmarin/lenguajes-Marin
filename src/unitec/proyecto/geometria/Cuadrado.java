e6708fb90a5d4cb337b44f08e96241b34b2fdbe2
T-107 <T-107@PC250>
2015-02-09 19:43:33 -0600

    Proyecto geometria

---
+++ src/unitec/proyecto/geometria/Cuadrado.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unitec.proyecto.geometria;
+
+/**
+ *
+ * @author T-107
+ */
+public class Cuadrado {
+    private float lado;
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+    
+    float calcularArea(){
+        float area=lado*lado;
+        return area;
+    }
+
+ 
+    
+}
